8d3c872cb0f673f511cb1034ad8f271f605ba9fc
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-04-14 00:34:46 +0100

    🚧 format

--- src/tailwind.config.mjs
+++ src/tailwind.config.mjs
@@ -5,7 +5,7 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			
+
 		},
 		fontFamily: {
 			sans: ['Orbitron', ...defaultTheme.fontFamily.sans],
